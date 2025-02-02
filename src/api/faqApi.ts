47d02315a263607c7fafd53c8b8170898e103d73
Utkarsh kumar <uk10234567@gmail.com>
2025-02-02 18:11:25 +0530

    added backedn link

--- src/api/faqApi.ts
+++ src/api/faqApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import type { FAQ } from '../types/faq';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://faq-system-eight.vercel.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
